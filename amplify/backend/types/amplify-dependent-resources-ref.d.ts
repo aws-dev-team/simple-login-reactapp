682b77378933ae75dc6c7cec5743104876508933
Roberto Hernández Montoya <roberto.hern83@gmail.com>
2022-01-09 22:38:52 -0600

    inclusion de storage y acceso a este

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -27,5 +27,11 @@ export type AmplifyDependentResourcesAttributes = {
             "ApiName": "string",
             "ApiId": "string"
         }
+    },
+    "storage": {
+        "storage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
